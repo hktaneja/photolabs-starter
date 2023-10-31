20465e8cd3c8f426ceee2ce5f84c715e64857544
hktaneja <hktaneja2020@gmail.com>
2023-10-31 16:07:03 -0500

    Create the Home Page Layout

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import TopNavigationBar from './components/TopNavigationBar';
+import PhotoList from './components/PhotoList';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
+      <TopNavigationBar />
+      <PhotoList />
       {/* Insert React */}
     </div>
   );
