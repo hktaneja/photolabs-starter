42d854b1c4810c5af83086202e11f0ffc2dcf1ff
hktaneja <hktaneja2020@gmail.com>
2023-10-31 20:40:23 -0500

    PhotoLabs stores the liked photos

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopNavigationBar from './components/TopNavigationBar';
 import PhotoList from './components/PhotoList';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
+  // Local state to store liked photos
+  const [likedPhotos, setLikedPhotos] = useState({});
+
+  // Function to toggle liking a photo
+  const toggleLike = (photoId) => {
+    // Check if the photo is already liked and toggle its state
+    setLikedPhotos((prevLikedPhotos) => ({
+      ...prevLikedPhotos,
+      [photoId]: !prevLikedPhotos[photoId],
+    }));
+  };
+
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar likedPhotos={likedPhotos} />
+      <PhotoList likedPhotos={likedPhotos} toggleLike={toggleLike} />
       {/* Insert React */}
     </div>
   );
